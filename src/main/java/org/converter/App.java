fab7d51a984e972557f80804288da62ead1076e9
cyril-ld <cyril.ledriant@gmail.com>
2014-04-12 22:41:29 +0200

    Ajout bidon pour test Github intégré eclipse

--- src/main/java/org/converter/App.java
+++ src/main/java/org/converter/App.java
@@ -8,5 +8,7 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		System.out.println("je teste l'utilisation de git depuis Eclipse ");
 	}
 }
