1c2ec158af279a7c4fe19d9a213f87fc03a267d7
cyril-ld <cyril.ledriant@gmail.com>
2014-05-01 02:36:55 +0200

    Ajout des interfaces

---
+++ src/main/java/org/converter/interfaces/Measure.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.converter.interfaces;
+
+/**
+ * Mesure de type générique...
+ * V pour une valeur, de type numérique (réelle)
+ * Q pour une quantité
+ * @author Cyril
+ */
+public interface Measure<V, Q> {
+    
+}
